b7076f15e734fb2b4b1d577949967df2d46f5175
Tengzhi Zhuo <tzhuo@umass.edu>
2021-11-04 11:57:23 -0400

    Fixed img not showing bug for contentPage and LoginPage and created a tmp server javascript file

---
+++ src/server/server.js
@@ -0,0 +1,21 @@
+'use strict';
+import * as express from 'express';
+
+let data = {};
+let app = express();
+
+//Write Functions Here
+//Example:
+// app.post('/userData', function(req, res) {
+//     let body = "";
+//     req.on('data', data => body += data);
+//     req.on('end', () => {
+           //Some data saving ...
+//         res.end();
+//     });
+// });
+
+//Server
+let server = app.listen(8080, function() {
+    
+});
