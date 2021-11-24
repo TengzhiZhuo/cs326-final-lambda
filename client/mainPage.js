5f341d49e980759e8a5f742ecc9f0e712623bae4
Hongxiang Wang <hongxiangwan@umass.edu>
2021-11-24 15:56:47 -0500

    mainpagejs

---
+++ client/mainPage.js
@@ -0,0 +1,24 @@
+'use strict';
+
+document.getElementById('mainpagesubmit').addEventListener('click', async () => {
+    const title = document.getElementById("mainpagetitle").value;
+    const content = document.getElementById("mainpagecontent").value;
+    const response = await fetch("/postsubmit", {
+        method: "post",
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify({ title : title, content : content})
+      });
+});
+
+window.onload = async function (){
+  const response1 = await fetch('/getPost');
+  const postArr = await response1.json();
+  document.getElementById('posttitle1').innerHTML = JSON.stringify((postArr[postArr.length - 1]).title);
+  document.getElementById('postcontent1').innerHTML = JSON.stringify((postArr[postArr.length - 1]).content);
+  document.getElementById('posttitle2').innerHTML = JSON.stringify((postArr[postArr.length - 2]).title);
+  document.getElementById('postcontent2').innerHTML = JSON.stringify((postArr[postArr.length - 2]).content);
+  document.getElementById('posttitle3').innerHTML = JSON.stringify((postArr[postArr.length - 3]).title);
+  document.getElementById('postcontent3').innerHTML = JSON.stringify((postArr[postArr.length - 3]).content);
+}
--- server.js
+++ server.js
@@ -1,143 +1,128 @@
 'use strict';
+
+//import { MongoClient } from 'mongodb';
 import express from 'express';
 import * as fs from 'fs';
 import { get } from 'http';
 import path from 'path';
-import { MongoClient } from 'mongodb';
+import {createServer} from 'http';
+import {parse} from 'url';
+import {join} from 'path';
+import {writeFile, readFileSync, existsSync} from 'fs';
+
 const __dirname = path.resolve();
 
+// Heroku
+// const url = "https://rocky-tundra-99275.herokuapp.com/";
+// const mongoDBurl = "mongodb+srv://tzhuo:cs326Lambda@cluster0.2noog.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true";
+// const client = new MongoClient(mongoDBurl);
+// await client.connect();
+// const db = client.db("UShare");
+// const collection1 = db.collection("user");
+// const collection2 = db.collection("post");
+// const collection3 = db.collection("comment");
+// const collection4 = db.collection("profile");
+
+// const user = await collection1.find().toArray();
+// const post = await collection2.find().toArray();
+// const comment = await collection3.find().toArray();
+// const profile = await collection4.find().toArray();
+
+// let data = {};
+// data['user'] = {};
+// data['post'] = {};
+// data['comment'] = {};
+// data['profile'] = {};
+// const JSONfile = './database.json';
+
 //Heroku
-const url = "https://rocky-tundra-99275.herokuapp.com/";
-const mongoDBurl = "mongodb+srv://tzhuo:cs326Lambda@cluster0.2noog.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true";
-const client = new MongoClient(mongoDBurl);
-await client.connect();
-const db = client.db("UShare");
-const collection1 = db.collection("user");
-const collection2 = db.collection("post");
-const collection3 = db.collection("comment");
-const collection4 = db.collection("profile");
+// "https:/  /rocky-tundra-99275.herokuapp.com/";
 
-const user = await collection1.find().toArray();
-const post = await collection2.find().toArray();
-const comment = await collection3.find().toArray();
-const profile = await collection4.find().toArray();
+let database;
+if (existsSync("database.json")) {
+    database = JSON.parse(readFileSync("database.json"));
+} else {
+    database = {
+        post: [],
+        user: [],
+        profile: []
+    };
+}
 
-let data = {};
-data['user'] = {};
-data['post'] = {};
-data['comment'] = {};
-data['profile'] = {};
-const JSONfile = './database.json';
+createServer(async (req, res) => {
+    const parsed = parse(req.url, true);
 
-//reload function
-function reload(filename) {
-    if (fs.existsSync(filename)) {
-        data = JSON.parse(fs.readFileSync(filename, { encoding: 'utf8' }));
-    }
-    else {
-        data = {};
-        data['user'] = {};
-        data['post'] = {};
-        data['comment'] = {};
-    }
-};
+    if (parsed.pathname === '/postsubmit') {
+        let body = '';
+        req.on('data', data => body += data);
+        req.on('end', () => {
+            const data = JSON.parse(body);
+            database.post.push({
+                title: data.title,
+                content: data.content
+            });
+            
+            writeFile("database.json", JSON.stringify(database), err => {
+                if (err) {
+                    console.err(err);
+                } else res.end();
+            });
+        });
+    } else if (parsed.pathname === '/gameScore') {
+        let body = '';
+        req.on('data', data => body += data);
+        req.on('end', () => {
+            const data = JSON.parse(body);
+            database.gameScores.push({
+                name: data.name,
+                score: data.score
+            });
+            
+            writeFile("database.json", JSON.stringify(database), err => {
+                if (err) {
+                    console.err(err);
+                } else res.end();
+            });
 
-reload(JSONfile);
-let app = express();
-app.use(express.json());
-app.use(express.urlencoded({
-    extended: true
-  }));
-//Static files
-app.use(express.static(__dirname + '/src/client'));
-app.use(express.static(__dirname + '/src/img'));
+            res.end(JSON.stringify(
+                database.post
+            ));
+        });
+    } else if (parsed.pathname === '/getPost') {
+        res.end(JSON.stringify(
+            database.post
+        ));
+    } else if (parsed.pathname === '/highestGameScores') {
+        res.end(JSON.stringify(
+            database.gameScores.sort((a, b) => b.score - a.score).filter((v, i) => i < 10)
+        ));
+    } else {
+        // If the client did not request an API endpoint, we assume we need to fetch and serve a file.
+        // This is terrible security-wise, since we don't check the file requested is in the same directory.
+        // This will do for our purposes.
+        const filename = parsed.pathname === '/' ? "index.html" : parsed.pathname.replace('/', '');
+        const path = join("client/", filename);
+        console.log("trying to serve " + path + "...");
+        if (existsSync(path)) {
+            if (filename.endsWith("html")) {
+                res.writeHead(200, {"Content-Type" : "text/html"});
+            }
+            if (filename.endsWith("js")) {
+                res.writeHead(200, {"Content-Type" : "text/javascript"});
+            }
+            if (filename.endsWith("css")) {
+                res.writeHead(200, {"Content-Type" : "text/css"});
+            }
+            // TODO you need to check for other filetypes and write the correct MIME type in the header.
+            // Without these, the browser is sometime incapable of interpreting the file as it should
+            // (Notably for JS files)
 
-//Write Functions Here
-//Example:
-// app.post('/userData', function(req, res) {
-//     let body = "";
-//     req.on('data', data => body += data);
-//     req.on('end', () => {
-           //Some data saving ...
-//         res.end();
-//     });
-// });
-
-//Render pages...
-app.get('', (req, res) => {
-    res.redirect('/mainPage');
-});
-
-app.get('/header', (req, res) => {
-    
-});
-
-//UserPage
-app.get('/userPage', (req, res) => {
-    res.sendFile(__dirname + '/src/client/userPage.html')
-});
-
-app.get('/userPageData', (req, res) => {
-    const key = data['user'][req.query['id']];
-    res.send(key);
-});
-
-app.post('/userPage', (req, res) => {
-    const elements = req.body;
-    const key = req.body['id'];
-    delete elements['id'];
-    for (const element in elements) {
-        if (elements[element] !== null) {
-            data['user'][key][element] = elements[element];
+            res.write(readFileSync(path));
+            res.end();
+        } else {
+            res.writeHead(404);
+            res.end();
         }
     }
-    const string = JSON.stringify(data);
-    fs.writeFileSync(JSONfile, string);
-    res.send();
-});
-
-
-//MainPage
-app.get('/mainPage', (req, res) => {
-    res.sendFile(__dirname + '/src/client/mainPage.html')
-});
-
-app.post('/mainPage', (req, res) => {
-    const elements = req.body;
-    const key = req.body['id'];
-    delete elements['id'];
-    for (const element in elements) {
-        data['post'][key][element] = elements[element];
-    }
-    const string = JSON.stringify(data);
-    fs.writeFileSync(JSONfile, string);
-    res.send();
-});
-
-//SignUp Page
-app.get('/signUp', (req, res) => {
-    res.sendFile(__dirname + '/src/client/SignUp.html')
-});
-
-app.post('/signUp/save', (req, res) => {
-    const elements = req.body;
-    const key = req.body['id'];
-    delete elements['id'];
-    for (const element in elements) {
-        data['profile'][key][element] = elements[element];
-    }
-    const string = JSON.stringify(data);
-    fs.writeFileSync(JSONfile, string);
-    res.send();
-});
-
-
-//Login Page
-app.get('/login', (req, res) => {
-    res.sendFile(path.resolve(__dirname + '/src/client/LogInPage.html'));
-});
-
-//Server
-app.listen(process.env.PORT || 8080);
-
-await client.close();
+}).listen(8080);
+//process.env.PORT || 8080
